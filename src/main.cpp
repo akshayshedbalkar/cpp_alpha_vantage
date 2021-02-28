1a95e565353c12dbee76fa62bf532e83c5c58966
Akshay Shedbalkar <akshayshedbalkar@gmail.com>
2021-02-28 14:18:48 +0100

    [Issue #8] Fixed
    Stock symbols can be read from stocks.config.

--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,55 @@
 #include "plot.h"
 #include "stock.h"
 
-int main() {
+static void read_stocks_from_config(const std::string &config_file_name, std::vector<std::string> &stock_names);
+
+int main()
+{
     std::string my_apikey = "demo";
     std::string my_cert_path = "E:\\programming\\cpp\\stocks\\extern\\certs\\curl-ca-bundle.crt";
 
-    //configure alphavantage and curl. This is only required once.
+    // configure alphavantage and curl. This is only required once.
     Api api(my_apikey, my_cert_path);
 
-    //configure gnuplot. This is required only once.
+    // configure gnuplot. This is required only once.
     Plot plot;
 
-    //Create Stock objects. For "Demo" apikey, only IBM works. Use own key for other symbols.
-    Stock stock1(&api, &plot, "IBM");
-    /* Stock stock2(&api, &plot, "GOOGL"); */
-    /* Stock stock3(&api, &plot, "NVDA"); */
+    // Create Stock objects. For "Demo" apikey, only IBM works. Use own key for
+    // other symbols.
+    /* std::vector<Stock> stocks{Stock(&api, &plot, "AMZN"), Stock(&api, &plot, "GOOGL"), Stock(&api, &plot, "NVDA")};
+     */
 
-    //Fetch stock data
-    stock1.fetch("TIME_SERIES_MONTHLY");
-    /* stock2.fetch("TIME_SERIES_MONTHLY"); */
-    /* stock3.fetch("TIME_SERIES_MONTHLY"); */
+    // You can also read stock symbol names from a file
+    std::vector<std::string> stock_names;
+    read_stocks_from_config("stocks.config", stock_names);
 
-    //show each plot of separate chart
-    stock1.display();
-    /* stock2.display(); */
-    /* stock3.display(); */
+    std::vector<Stock> stocks;
+    for (const auto &stock_name : stock_names)
+    {
+        stocks.push_back(Stock(&api, &plot, stock_name));
+    }
 
-    //show all plots on one chart
+    // Fetch stock data
+    for (auto &stock : stocks)
+    {
+        stock.fetch("TIME_SERIES_DAILY");
+    }
+
+    // show each plot of separate chart
+    /* for (const auto &stock : stocks) */
+    /* { */
+    /*     stock.display(); */
+    /* } */
+
+    // show all plots on one chart
     plot.display();
 
     return 0;
+}
+
+static void read_stocks_from_config(const std::string &config_file_name, std::vector<std::string> &stock_names)
+{
+    std::ifstream config_file(config_file_name.c_str());
+    std::copy(std::istream_iterator<std::string>(config_file), std::istream_iterator<std::string>(),
+              back_inserter(stock_names));
 }
